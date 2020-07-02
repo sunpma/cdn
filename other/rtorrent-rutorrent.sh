dad9866ddf1f1ea100f2116df01dc4e2df56faf6
sunpma <45225037+sunpma@users.noreply.github.com>
2020-07-02 19:45:27 +0800

    Delete rtorrent-rutorrent.sh

--- other/rtorrent-rutorrent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get autoremove --purge apache* -y
-apt-get autoremove --purge mysql* -y
-apt-get update
-apt-get install nano screen -y
-wget --no-check-certificate -O autodl-setup http://sourceforge.net/projects/autodl-irssi/files/autodl-setup/download
-sh autodl-setup
-sed -e '7s/30/5/g' /var/rutorrent/rutorrent/plugins/rss/conf.php
